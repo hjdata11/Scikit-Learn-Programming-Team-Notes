3a81a7528d9e445bc463ec7f632b7ab87e82d172
Hunjin Chung <>
2021-05-30 05:40:33 +0000

    trainset, testset 분리해서 만들기

---
+++ test_train_split.py
@@ -0,0 +1,9 @@
+# [trainset testset 분리]
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris_dataset = load_iris()
+
+# random_state : 이 함수를 여러 번 실행해도 결과가 똑같이 나오도록 유사 난수 생성기에 넣을 난수 초깃값 전달
+X_train, X_test, Y_train, Y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
