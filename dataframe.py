05b49f4e56564f670a2501a02a0af9e2f7962e8d
Hunjin Chung <>
2021-06-02 02:25:24 +0000

    인덱싱, 데이터 프레임

---
+++ dataframe.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+titanic_df = pd.read_csv('train.csv')
+
+# 총 데이터 건수와 데이터 타입, NULL 건수 확인
+titanic_df.info()
+# 개략적 분포도 확인
+titanic_df.describe()
+# 데이터 분포도 확인
+titanic_df['Pclass'].value_counts()
+
+# DataFrame <-> array
+col_name = ['col1', 'col2', 'col3']
+list2 = [[1, 2, 3], [11, 12, 13]]
+dict2 = {'col1':[1, 11], 'col2':[2, 22], 'col3':[3, 33]}
+
+array2 = np.array(list2)
+df_list2 = pd.DataFrame(list2, columns=col_name)
+print(df_list2)
+df_array2 = pd.DataFrame(array2, columns=col_name)
+print(df_array2)
+df_dict = pd.DataFrame(dict2)
+print(df_dict)
+
+# ndarray
+array3 = df_dict.values
+print(array3)
+# list
+list3 = df_dict.values.tolist()
+print(list3)
+# dict
+dict3 = df_dict.to_dict('list')
+print(dict3)
+
+# Dataframe 생성 수정
+titanic_df['Age_by_10'] = titanic_df['Age']*10
+titanic_df['Family_No'] = titanic_df['SibSp'] + titanic_df['Parch'] + 1
+titanic_df.head(3)
+# Dataframe 삭제
+drop_result = titanic_df.drop(['Age_by_10', 'Family_No'], axis = 1, inplace=True)
+titanic_df.head(3)
--- index_sort.py
+++ index_sort.py
@@ -13,3 +13,9 @@ score_array = np.array([78, 95, 84, 98, 88])
 
 sort_indices_asc = np.argsort(score_array)
 print(name_array[sort_indices_asc])
+
+# ix 연산 유형
+# data_df.ix[0, 0], data_df.ix['one', 0], data_df.ix[3, 'Name'], data_df.ix[0:2, [0, 1]], data_df[0:2, [0:3]], data_df.ix[0:3, ['Name', 'Year']], data_df.ix[:], data_df.ix[:, :], data_df[data_df.Year >= 2014]
+
+# column 이름 변환
+data_df = data_df.rename(columns={'index':'old_index'})
