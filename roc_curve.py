064ba134ec697c58efebf6204cfda8ffe00137c6
Hunjin Chung <>
2021-06-06 01:14:26 +0000

    ROC_CURVE

--- roc_curve.py
+++ roc_curve.py
@@ -1,35 +1,16 @@
-# confusion matrix
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+# ROC_CURVE
+from sklearn.metrics import roc_curve
 
-def get_clf_eval(y_test, pred):
-    confusion = confusion_matrix(y_test, pred)
-    accuracy = accuracy_score(y_test, pred)
-    precision = precision_score(y_test, pred)
-    recall = recall_score(y_test, pred)
-    print(confusion)
-    print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f}'.format(accuracy, precision, recall))
+def roc_curve_plot(y_test, pred_proba_c1):
 
-# ROC_Curve
-import matplotlib.pyplot as plt
-import numpy as np
-%matplotlib inline
-from sklearn.metrics import precision_recall_curve
-
-def precision_recall_curve_plot(y_test, pred_proba_c1):
-    precisions, recalls, thresholds = precision_recall_curve(y_test, pred_proba_c1)
+    fprs, tprs, thresholds = roc_curve(y_test, pred_proba_c1)
+    plt.plot(fprs, tprs, label='ROC')
+    plt.plot([0,1], [0,1], 'k--', label='Random')
     
-    plt.figure(figsize=(8, 6))
-    threshold_boundary = thresholds.shape[0]
-    plt.plot(thresholds, precisions[0:threshold_boundary], linestyle='--', label='precision')
-    plt.plot(thresholds, recalls[0:threshold_boundary], label='recall')
-    
-    # threshold 값 X 축의 Scale을 0.1 단위로 변경
     start, end = plt.xlim()
     plt.xticks(np.round(np.arange(start, end, 0.1), 2))
+    plt.xlim(0, 1); plt.ylim(0, 1)
+    plt.xlabel('FPR(1 - Sensitivity)'); plt.ylabel('TPR(Recall)')
+    plt.legend()
     
-    # x축 ,y축 label과 legend, 그리고 grid 설정
-    plt.xlabel('Threshold value'); plt.ylabel('Precision and Recall value')
-    plt.legend(); plt.grid()
-    plt.show()
-    
-precision_recall_curve_plot(y_test, lr_clf.predict_proba(X_test)[:, 1])
+roc_curve_plot(y_test, lr_clf.predict_proba(X_test)[:, 1])
