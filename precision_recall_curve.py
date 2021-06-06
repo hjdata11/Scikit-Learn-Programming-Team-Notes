2cb4b7228bcc85c5f53a8f642cdb91a69952537e
Hunjin Chung <>
2021-06-06 04:25:17 +0000

    ROC_CURVE

--- precision_recall_curve.py
+++ precision_recall_curve.py
@@ -1,5 +1,9 @@
 # confusion matrix
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score, precision_recall_curve
+from sklearn.preprocessing import Binarizer
+
+pred_proba = lr_clf.predict_proba(X_test)
+thresholds = [0.4, 0.45, 0.50, 0.55, 0.60]
 
 def get_clf_eval(y_test, pred, pred_proba):
     confusion = confusion_matrix(y_test, pred)
@@ -10,6 +14,15 @@ def get_clf_eval(y_test, pred, pred_proba):
     roc_auc = roc_auc_score(y_test, pred_proba)
     print(confusion)
     print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f}, F1: {3:.4f}, AUC: {4:.4f}'.format(accuracy, precision, recall, f1, roc_auc))
+
+def get_eval_by_threshold(y_test, pred_proba_c1, thresholds):
+    for custom_threshold in thresholds:
+        binarizer = Binarizer(threshold=custom_threshold).fit(pred_proba_c1)
+        custom_predict = binarizer.transform(pred_proba_c1)
+        print('임계값:', custom_threshold)
+        get_clf_eval(y_test, custom_predict, pred_proba[:, 1])
+        
+get_eval_by_threshold(y_test, pred_proba[:, 1].reshape(-1, 1), thresholds)
     
 
 # precision_recall_curve
